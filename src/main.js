613421b899a26632ef491f2a2f1829ad708b1573
ndhphuc <ndanghoangphuc@gmail.com>
2023-10-11 17:02:30 +0700

    update src as

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import PrimeVue from 'primevue/config';
-import VueSweetalert2 from 'vue-sweetalert2';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import VueSweetalert2 from "vue-sweetalert2";
 import "primevue/resources/themes/lara-light-indigo/theme.css";
-import 'sweetalert2/dist/sweetalert2.min.css';
+import "sweetalert2/dist/sweetalert2.min.css";
 
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.bundle.min";
+import "primeflex/primeflex.scss";
+import App from "./App.vue";
+import router from "./router";
 
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
-
-app.use(createPinia())
+app.use(createPinia());
 app.use(PrimeVue);
 app.use(router);
 app.use(VueSweetalert2);
 
-app.mount('#app');
+app.mount("#app");
