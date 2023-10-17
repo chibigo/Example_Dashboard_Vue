20e55f5eda5e532a3dc2c19ff8b80ad4ad75caba
Quynguyen <01208122024quy@gmail.com>
2023-10-17 16:40:00 +0700

    Update Product Sku

--- src/stores/product.js
+++ src/stores/product.js
@@ -1,113 +1,170 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia'
 import {
   getListProduct,
   createProduct,
   createProductSku,
   updateProduct,
   actionProduct,
-} from "@/api/product";
-import Swal from "sweetalert2/dist/sweetalert2.js";
+  getListProductSku,
+  productSkuAction,
+  updateProductSkuAction
+} from '@/api/product'
+import Swal from 'sweetalert2/dist/sweetalert2.js'
 
-export const useProductStore = defineStore("product", {
+export const useProductStore = defineStore('product', {
   state: () => {
-    return { listProduct: [], loading: false };
+    return { listProduct: [], loading: false, listProductSku: [] }
   },
   getters: {},
   actions: {
     async getListProductAction(params) {
       try {
-        this.loading = true;
-        const res = await getListProduct(params);
+        this.loading = true
+        const res = await getListProduct(params)
         if (res.success) {
-          this.listProduct = res.data;
+          this.listProduct = res.data
         } else {
-          this.listProduct = [];
+          this.listProduct = []
         }
-        this.loading = false;
+        this.loading = false
       } catch (e) {}
     },
     async createProductAction(data) {
       try {
-        const res = await createProduct(data);
+        const res = await createProduct(data)
         Swal.fire({
-          position: "center",
-          icon: "success",
+          position: 'center',
+          icon: 'success',
           title: res.message,
           showConfirmButton: false,
-          timer: 1500,
-        });
-        await this.getListProductAction();
+          timer: 1500
+        })
+        await this.getListProductAction()
       } catch (err) {
         Swal.fire({
-          title: "Error!",
+          title: 'Error!',
           text: err,
-          icon: "error",
-        });
+          icon: 'error'
+        })
       }
     },
     async updateProductAction(data) {
       try {
-        const res = await updateProduct(data);
+        const res = await updateProduct(data)
         Swal.fire({
-          position: "center",
-          icon: "success",
+          position: 'center',
+          icon: 'success',
           title: res.message,
           showConfirmButton: false,
-          timer: 1500,
-        });
-        await this.getListProductAction();
+          timer: 1500
+        })
+        await this.getListProductAction()
       } catch (err) {
         Swal.fire({
-          title: "Error!",
+          title: 'Error!',
           text: err,
-          icon: "error",
-        });
+          icon: 'error'
+        })
       }
     },
     async actionProductAction(productCode, type) {
       try {
-        const res = await actionProduct(productCode, type);
+        const res = await actionProduct(productCode, type)
         if (res.success) {
           Swal.fire({
-            position: "center",
-            icon: "success",
+            position: 'center',
+            icon: 'success',
             title: res.message,
             showConfirmButton: false,
-            timer: 1500,
-          });
-          await this.getListProductAction();
+            timer: 1500
+          })
+          await this.getListProductAction()
         } else {
           Swal.fire({
-            title: "Error!",
-            text: "Error!",
-            icon: "error",
-          });
+            title: 'Error!',
+            text: 'Error!',
+            icon: 'error'
+          })
         }
       } catch (e) {
         Swal.fire({
-          title: "Error!",
+          title: 'Error!',
           text: e,
-          icon: "error",
-        });
+          icon: 'error'
+        })
       }
     },
     async createProductSkuAction(data) {
       try {
-        const res = await createProductSku(data);
+        await createProductSku(data)
+
         Swal.fire({
-          position: "center",
-          icon: "success",
-          title: "Success",
+          position: 'center',
+          icon: 'success',
+          title: 'Success',
           showConfirmButton: false,
-          timer: 1500,
-        });
+          timer: 1500
+        })
+        await this.getListProductSkuAction()
       } catch (err) {
         Swal.fire({
-          title: "Error!",
+          title: 'Error!',
           text: err,
-          icon: "error",
-        });
+          icon: 'error'
+        })
       }
     },
-  },
-});
+    async getListProductSkuAction(params) {
+      try {
+        const res = await getListProductSku(params)
+        if (res.success) {
+          this.listProductSku = res.data
+        } else {
+          this.listProductSku = []
+        }
+      } catch (e) {}
+    },
+    async blockDeleteProductSkuAction(params) {
+      try {
+        const res = await productSkuAction(params)
+        if (res.success) {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Success',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          await this.getListProductSkuAction()
+        }
+      } catch (err) {
+        Swal.fire({
+          title: 'Error!',
+          text: err,
+          icon: 'error'
+        })
+      }
+    },
+    async updateProductSkuAction(data) {
+      try {
+        const res = await updateProductSkuAction(data)
+        if (res.success) {
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Success',
+            showConfirmButton: false,
+            timer: 1500
+          })
+          await this.getListProductSkuAction()
+        }
+      } catch (err) {
+        Swal.fire({
+          title: 'Error!',
+          text: err,
+          icon: 'error'
+        })
+      }
+    }
+  }
+})
