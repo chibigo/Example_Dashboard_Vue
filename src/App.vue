b2a28c18f87caf47d85a4f7fa6e18b1c5db56c73
ndhphuc <ndanghoangphuc@gmail.com>
2023-10-17 09:35:31 +0700

    update search product

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <RouterView class="router-view" />
 </template>
 <script setup>
-import { RouterView } from 'vue-router'
+import { RouterView } from "vue-router";
 </script>
 
 <style scoped>
--- src/router/Route.js
+++ src/router/Route.js
@@ -1,60 +1,60 @@
 export const routes = [
   {
-    path: '/',
-    name: '',
-    component: () => import('@/components/Layout/index.vue'),
+    path: "/",
+    name: "",
+    component: () => import("@/layout/index.vue"),
     meta: {
-      checkAuth: true
+      checkAuth: true,
     },
     children: [
       {
-        path: '/',
-        name: 'dashboard',
-        component: () => import('@/views/DashBoard/index.vue')
+        path: "/",
+        name: "dashboard",
+        component: () => import("@/views/DashBoard/index.vue"),
       },
       {
-        path: '/category',
-        name: 'category',
-        component: () => import('@/views/Category/CategoryView.vue')
+        path: "/category",
+        name: "category",
+        component: () => import("@/views/Category/CategoryView.vue"),
       },
       {
-        path: '/product',
-        name: 'product',
-        component: () => import('@/views/Product/ProductView.vue')
+        path: "/product",
+        name: "product",
+        component: () => import("@/views/Product/ProductView.vue"),
       },
       {
-        path: '/member',
-        name: 'member',
-        component: () => import('@/views/Member/index.vue')
+        path: "/member",
+        name: "member",
+        component: () => import("@/views/Member/index.vue"),
       },
       {
-        path: '/library',
-        name: 'library',
-        component: () => import('@/views/LibaryFile/index.vue')
+        path: "/library",
+        name: "library",
+        component: () => import("@/views/LibaryFile/index.vue"),
       },
       {
-        path: '/setting',
-        name: 'setting',
-        component: () => import('@/views/SettingView.vue')
-      }
-    ]
+        path: "/setting",
+        name: "setting",
+        component: () => import("@/views/SettingView.vue"),
+      },
+    ],
   },
   {
-    path: '/login',
-    name: 'login',
-    component: () => import('@/views/Login/index.vue'),
+    path: "/login",
+    name: "login",
+    component: () => import("@/views/Login/index.vue"),
     meta: {
-      requestAuthen: true
-    }
+      requestAuthen: true,
+    },
   },
   {
-    path: '/404',
-    name: 'PageNotFound',
-    component: () => import('../views/Error404.vue')
+    path: "/404",
+    name: "PageNotFound",
+    component: () => import("../views/Error404.vue"),
   },
   {
-    path: '/:catchAll(.*)',
-    redirect: '404'
-  }
-]
+    path: "/:catchAll(.*)",
+    redirect: "404",
+  },
+];
 // export default routes
